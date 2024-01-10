2d59a3d77df6d8f5fcbb5b7d0a3196052c40e01a
yingkie17 <yingkie17@gmail.com>
2024-01-09 22:13:46 -0400

    Se agrego el SSL a Nodejs

--- config/config.js
+++ config/config.js
@@ -7,7 +7,8 @@ const options = {
 const pgp = require('pg-promise')(options);
 
 const db = pgp({
-  connectionString: 'postgres://yingkie17:CnkZkgqbeSAMiB5BfR8RaqiHFKnBVJ1a@dpg-cmdee76d3nmc73djegrg-a.oregon-postgres.render.com/bienesraices_db_wm7t' + "?sslmode=require",
+  connectionString: 'postgres://yingkie17:CnkZkgqbeSAMiB5BfR8RaqiHFKnBVJ1a@dpg-cmdee76d3nmc73djegrg-a.oregon-postgres.render.com/bienesraices_db_wm7t' + "?sslmode=require", 
+  ssl: true
 });
 
 module.exports = db;
